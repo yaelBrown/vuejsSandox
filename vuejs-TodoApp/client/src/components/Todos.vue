911392f96f67109685ef77585926696cfa0d8097
Yael Brown <yaelrbrown@gmail.com>
2020-01-26 22:15:14 -0600

    not sure why app is broken.

---
+++ vuejs-TodoApp/client/src/components/Todos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todos",
+  data: {
+      [
+        {
+          "id": 1,
+          "name": "Todo Item",
+          "isCompleted": false
+        },
+        {
+          "id": 2,
+          "name": "Todo Item",
+          "isCompleted": false
+        },
+        {
+          "id": 3,
+          "name": "Todo Item",
+          "isCompleted": false
+        }
+      ]
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
