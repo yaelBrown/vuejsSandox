150b77a731ad6b4955b1b1b69e9f6013d9c5d0e9
Yael Brown <yaelrbrown@gmail.com>
2020-09-07 21:37:41 -0500

    deleting files

--- KgsToLbs/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int weightInPounds = 223;
-        double PoundsInKgs = (double) (weightInPounds * 0.45359237);
-        System.out.println(PoundsInKgs);
-    }
-
-}
